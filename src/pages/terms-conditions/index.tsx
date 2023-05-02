d84be5100652b94d904f863ed2fcd94bec8ad069
Mukhammadyusuf Abdurakhimov <papa@uwussi.moe>
2023-05-01 21:02:13 -0700

    Terms

---
+++ src/pages/terms-conditions/index.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+
+const TermsConditions = () => {
+  return (
+    <div className="p-5 md:px-32">
+      <h2 className="text-3xl font-bold py-4">Terms & Conditions</h2>
+      By using the site, these terms will automatically apply to you – you
+      should make sure therefore that you read them carefully before using the
+      site. You’re not allowed to copy or modify the app, any part of the app,
+      or our trademarks in any way. You’re not allowed to attempt to extract the
+      source code of the app, and you also shouldn’t try to translate the app
+      into other languages or make derivative versions. The app itself, and all
+      the trademarks, copyright, database rights, and other intellectual
+      property rights related to it, still belong to Optochka. Optochka is
+      committed to ensuring that the site is as useful and efficient as
+      possible. For that reason, we reserve the right to make changes to the
+      site or to charge for its services, at any time and for any reason. We
+      will never charge you for the site or its services without making it very
+      clear to you exactly what you’re paying for. The Optochka site stores and
+      processes personal data that you have provided to us, to provide our
+      Service. It’s your responsibility to keep your phone and access to the
+      site secure. The site does use third-party services that declare their
+      Terms and Conditions. Link to Terms and Conditions of third-party service
+      providers used by the app Google Play Services Google Analytics Facebook
+      Telegram
+      <br />
+      You should be aware that there are certain things that Optochka will not
+      take responsibility for. Certain functions of the app will require the app
+      to have an active internet connection. The connection can be Wi-Fi or
+      provided by your mobile network provider, but Optochka cannot take
+      responsibility for the app not working at full functionality if you don’t
+      have access to Wi-Fi, and you don’t have any of your data allowance left.
+      If you’re using the site outside an area with Wi-Fi, you should remember
+      that the terms of the agreement with your mobile network provider will
+      still apply. As a result, you may be charged by your mobile provider for
+      the cost of data for the duration of the connection while accessing the
+      site, or other third-party charges. In using the site, you’re accepting
+      responsibility for any such charges, including roaming data charges if you
+      use the app outside your home territory (i.e. region or country) without
+      turning off data roaming. If you are not the bill payer for the device on
+      which you’re using the site, please be aware that we assume that you have
+      received permission from the bill payer for using the site. Along the same
+      lines, site cannot always take responsibility for the way you use the site
+      i.e. You need to make sure that your device stays charged – if it runs out
+      of battery, and you can’t turn it on to avail the Service, site cannot
+      accept responsibility. With respect to Optochka’s responsibility for your
+      use of the site, when you’re using the app, it’s important to bear in mind
+      that although we endeavor to ensure that it is updated and correct at all
+      times, we do rely on third parties to provide information to us so that we
+      can make it available to you. Optochka accepts no liability for any loss,
+      direct or indirect, you experience as a result of relying wholly on this
+      functionality of the site. Optochka does not promise that it will always
+      update the site so that it is relevant to you and/or works. However, you
+      promise to always accept updates to the site when offered to you, We may
+      also wish to stop providing the site, and may terminate use of it at any
+      time without giving notice of termination to you.
+      <h3 className="text-lg font-bold py-4">
+        Changes to This Terms and Conditions
+      </h3>
+      We may update our Terms and Conditions from time to time. Thus, you are
+      advised to review this page periodically for any changes. We will notify
+      you of any changes by posting the new Terms and Conditions on this page.
+      These terms and conditions are effective as of 2023-05-01
+      <h3 className="text-lg font-bold py-4">Contact Us</h3>
+      If you have any questions or suggestions about our Terms and Conditions,
+      do not hesitate to contact us at infomollysuz@gmail.com.
+    </div>
+  );
+};
+
+export default TermsConditions;
